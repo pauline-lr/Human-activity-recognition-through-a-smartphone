2d8d15f4bdd48317b53a9c28792b7e119bf8888d
Jonathan Smith <etu43752@henallux.be>
2021-08-03 16:21:09 +0200

    up

---
+++ PNDI_in_C/cmake-build-debug/CMakeFiles/PNDI_in_C.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PNDI_in_C.dir/phase_1.c.o"
+  "PNDI_in_C"
+  "PNDI_in_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PNDI_in_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
