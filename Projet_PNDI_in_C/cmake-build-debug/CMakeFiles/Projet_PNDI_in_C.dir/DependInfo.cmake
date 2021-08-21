058ff48b52f15a7f1a75808c8b77cbb782121cc3
pauline-lr <etu40989@henallux.be>
2021-08-21 20:13:10 +0200

    up

---
+++ Projet_PNDI_in_C/cmake-build-debug/CMakeFiles/Projet_PNDI_in_C.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/IG 2020-2021/Q3/Projet PNDI AOUT 2021/Projet_PNDI_in_C/main.c" "D:/IG 2020-2021/Q3/Projet PNDI AOUT 2021/Projet_PNDI_in_C/cmake-build-debug/CMakeFiles/Projet_PNDI_in_C.dir/main.c.obj"
+  "D:/IG 2020-2021/Q3/Projet PNDI AOUT 2021/Projet_PNDI_in_C/phase_1.c" "D:/IG 2020-2021/Q3/Projet PNDI AOUT 2021/Projet_PNDI_in_C/cmake-build-debug/CMakeFiles/Projet_PNDI_in_C.dir/phase_1.c.obj"
+  "D:/IG 2020-2021/Q3/Projet PNDI AOUT 2021/Projet_PNDI_in_C/phase_2.c" "D:/IG 2020-2021/Q3/Projet PNDI AOUT 2021/Projet_PNDI_in_C/cmake-build-debug/CMakeFiles/Projet_PNDI_in_C.dir/phase_2.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
